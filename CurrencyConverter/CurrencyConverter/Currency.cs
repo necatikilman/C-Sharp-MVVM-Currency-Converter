1b70e3cb76de0e2be7ff6e740344aff6740b1aeb
necatikilman <necati.kilman@hotmail.com>
2022-05-31 15:19:54 +0200

    Start point set up.

---
+++ CurrencyConverter/CurrencyConverter/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+
+    internal enum Currency
+    {
+        [Description("United States Dollar")]
+        USD,
+        [Description("Euro")]
+        EUR,
+        [Description("Japanese Yen")]
+        JPY,
+        [Description("Pound sterling")]
+        GBP,
+        [Description("Australian Dollar")]
+        AUD
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CurrencyConverter
+{
+    internal class MainViewModel
+    {
+        public Array Currencies => Enum.GetValues(typeof(Currency));
+
+    }
+}
